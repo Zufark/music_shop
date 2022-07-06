199db4df52d678d0ee07ca7719679b4b05be14ef
Zufark <zoufar@udm.ru>
2022-07-06 22:51:51 +0400

    initial commit

---
+++ app/src/main/java/com/zufar/musicshop/MainActivity.java
@@ -0,0 +1,32 @@
+package com.zufar.musicshop;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    int quantity = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void increaseQuantity(View view) {
+        quantity = quantity + 1;
+        TextView quantityTextView = findViewById(R.id.quantityTextView);
+        quantityTextView.setText("" + quantity);
+    }
+
+    public void decreaseQuantity(View view) {
+        quantity = quantity - 1;
+        if (quantity < 0) {
+            quantity = 0;
+        }
+        TextView quantityTextView = findViewById(R.id.quantityTextView);
+        quantityTextView.setText("" + quantity);
+    }
+}
